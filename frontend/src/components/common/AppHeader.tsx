e1736c24f9f8ce3f0afbb22e4136a1d46de065cf
luckylooky2 <luckylooky2@naver.com>
2023-04-19 21:54:48 +0900

    #1 rename : NavBar -> AppHeader

---
+++ frontend/src/components/common/AppHeader.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import MainRoute from "Route";
+import AppBar from "./AppBar";
+import FriendDrawer from "./FriendDrawer";
+import ChattingDrawer from "./ChattingDrawer";
+import { ChattingDrawerWidth } from "constant";
+
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginRight: -ChattingDrawerWidth,
+  ...(open && {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: 0,
+  }),
+}));
+
+export default function AppHeader() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar open={open} setOpen={setOpen} />
+      <FriendDrawer />
+      <Main open={open}>
+        <Box component="main" sx={{ flexGrow: 1, p: 5 }}>
+          <Routes>
+            <Route path="/*" element={<MainRoute />} />
+          </Routes>
+        </Box>
+      </Main>
+      <ChattingDrawer open={open} setOpen={setOpen} />
+    </Box>
+  );
+}
