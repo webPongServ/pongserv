fa0e2d022d0c993b3b82b72d1a806563c150ae1a
luckylooky2 <luckylooky2@naver.com>
2023-04-22 01:43:29 +0900

    #5 feat : achievement list sample

--- frontend/src/components/profile/AchievementList.tsx
+++ frontend/src/components/profile/AchievementList.tsx
@@ -1,19 +1,63 @@
+import { useState } from "react";
+
 import TabPanel from "@mui/joy/TabPanel";
+import Card from "@mui/joy/Card";
 import Typography from "@mui/joy/Typography";
+import "styles/global.scss";
+
+export type Achievement = {
+  achvTitle: string;
+  achvContent: string;
+  achvImg: string;
+};
 
 const Achievements = () => {
+  const [achievementList, setAchievementList] = useState<Achievement[]>([
+    {
+      achvTitle: "1승 업적",
+      achvContent: "1승 업적에 관한 내용입니다.",
+      achvImg: "../image.png",
+    },
+    {
+      achvTitle: "42승 업적",
+      achvContent: "42승 업적에 관한 내용입니다.",
+      achvImg: "../image.png",
+    },
+    {
+      achvTitle: "1승 업적",
+      achvContent: "1승 업적에 관한 내용입니다.",
+      achvImg: "../image.png",
+    },
+    {
+      achvTitle: "42승 업적",
+      achvContent: "42승 업적에 관한 내용입니다.",
+      achvImg: "../image.png",
+    },
+  ]);
   return (
-    <TabPanel value={1} sx={{ p: 3 }}>
-      <Typography level="inherit">
-        Best for professional developers building enterprise or data-rich
-        applications.
-      </Typography>
-      <Typography textColor="primary.400" fontSize="xl3" fontWeight="xl" my={1}>
-        $15{" "}
-        <Typography fontSize="sm" textColor="text.secondary" fontWeight="md">
-          / dev / month
-        </Typography>
-      </Typography>
+    <TabPanel
+      value={1}
+      sx={{ p: 3, display: "flex" }}
+      className="flex-container profile-fullcontainer"
+    >
+      <div className="flex-wrap-container">
+        {achievementList.map((value) => {
+          return (
+            <Card
+              variant="outlined"
+              className="achievement-container flex-container gap"
+            >
+              <div className="flex-container column">
+                <img src={value.achvImg} alt="achievement_image" />
+                <div>
+                  <Typography>{value.achvTitle}</Typography>
+                  <Typography>{value.achvContent}</Typography>
+                </div>
+              </div>
+            </Card>
+          );
+        })}
+      </div>
     </TabPanel>
   );
 };
