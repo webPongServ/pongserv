58797709440a284bce164bcf1e4b58b36e3eec17
Refi Go <64242999+refigo@users.noreply.github.com>
2023-04-23 19:45:42 +0900

    feat: Set db-manager with db-chats-manager and db-games-manager

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { GamesModule } from './games/games.module';
+import { DbManagerModule } from './db-manager/db-manager.module';
 
 @Module({
-  imports: [UserModule, GamesModule],
+  imports: [UserModule, GamesModule, DbManagerModule],
   controllers: [AppController],
   providers: [AppService],
 })
