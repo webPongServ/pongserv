41d7980af25c78649aa6f2c986eb3d36d5d3e946
SeungWoonSong <susong@student.42seoul.kr>
2023-04-23 19:42:01 +0900

    INIT: chats service, module, controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { GamesModule } from './games/games.module';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
-  imports: [UserModule, GamesModule],
+  imports: [UserModule, GamesModule, ChatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
