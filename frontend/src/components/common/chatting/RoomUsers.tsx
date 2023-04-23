4c55a33ca037d9f2a6a1d77062b6d48e76e8808d
luckylooky2 <luckylooky2@naver.com>
2023-04-23 21:28:25 +0900

    #7 FEAT : room users list modal frame + css

---
+++ frontend/src/components/common/chatting/RoomUsers.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+import { Box } from "@mui/material";
+import Select from "@mui/joy/Select";
+import Option from "@mui/joy/Option";
+import "styles/global.scss";
+import "styles/Chatting.scss";
+import IconButton from "@mui/joy/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+
+type HandleRoomDetail = {
+  setRoomStatus: Function;
+};
+
+const RoomUsers = (props: HandleRoomDetail) => {
+  const [selected, setSelected] = useState<string>("users");
+  return (
+    <>
+      <Box className="flex-container" sx={{ height: "10%" }}>
+        <Select
+          defaultValue={selected}
+          // onChange={(e) => {
+          //   if (e) {
+          //     const target: HTMLInputElement = e.target as HTMLInputElement;
+          //     props.setPartyForm({
+          //       ...props.partyForm,
+          //       category: target.outerText,
+          //     });
+          //   }
+          // }}
+        >
+          <Option
+            value="users"
+            onClick={() => {
+              setSelected("users");
+            }}
+          >
+            유저 목록
+          </Option>
+          <Option
+            value="bans"
+            onClick={() => {
+              setSelected("bans");
+            }}
+          >
+            차단 목록
+          </Option>
+        </Select>
+        <IconButton
+          sx={{ marginLeft: "auto" }}
+          onClick={() => {
+            props.setRoomStatus("chat");
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </Box>
+      <Box
+        className="flex-container align-normal justify-normal"
+        sx={{
+          flexDirection: "column",
+          height: "90%",
+          gap: 3,
+          marginTop: "5%",
+        }}
+      >
+        <Box
+          sx={{
+            height: "100%",
+            p: 1,
+            overflow: "auto",
+          }}
+          className="users-box"
+        >
+          {selected === "users" ? (
+            <Box sx={{ p: 3, height: "100%", overflow: "auto" }}>
+              유저 목록입니다.
+            </Box>
+          ) : (
+            <Box sx={{ p: 3, height: "100%", overflow: "auto" }}>
+              차단 목록입니다.
+            </Box>
+          )}
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default RoomUsers;
