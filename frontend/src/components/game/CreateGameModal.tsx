1619c7b0a47e2a048be425bd94f7e542aac9441c
luckylooky2 <luckylooky2@naver.com>
2023-04-25 22:18:47 +0900

    #30 WIP : create game modal

---
+++ frontend/src/components/game/CreateGameModal.tsx
@@ -0,0 +1,61 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+import { Typography } from "@mui/material";
+import { Button } from "@mui/joy";
+import Modal from "@mui/joy/Modal";
+import ModalDialog from "@mui/joy/ModalDialog";
+import { Box } from "@mui/material";
+import "styles/Game.scss";
+
+type CreateGameModalProps = {
+  roomStatus: string;
+  setRoomStatus: Function;
+};
+
+const CreateGameModal = (props: CreateGameModalProps) => {
+  const navigate = useNavigate();
+  const [roomID, setRoomID] = useState<string>("203404250001");
+  return (
+    <Modal
+      open={props.roomStatus === "create-game"}
+      onClose={() => props.setRoomStatus("game")}
+    >
+      <ModalDialog className="game-modal" variant="outlined">
+        <Typography
+          sx={{ height: "10%", fontSize: "30px", margin: "auto auto" }}
+        >
+          일반 게임
+        </Typography>
+        <Box
+          className="flex-container direction-column"
+          sx={{ height: "70%", gap: 1 }}
+        >
+          <Typography>입장과 동시에 게임이 시작됩니다.</Typography>
+          <Typography>
+            <b>입장하시겠습니까?</b>
+          </Typography>
+        </Box>
+        <Box className="modal-button-group">
+          <Button
+            className="medium-size"
+            onClick={() => {
+              navigate(`/game/${roomID}`);
+            }}
+          >
+            생성
+          </Button>
+          <Button
+            variant="outlined"
+            className="medium-size"
+            onClick={() => props.setRoomStatus("game")}
+          >
+            취소
+          </Button>
+        </Box>
+      </ModalDialog>
+    </Modal>
+  );
+};
+
+export default CreateGameModal;
