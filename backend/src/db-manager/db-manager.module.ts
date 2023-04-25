5c464cc24143f67a681b5a313cc1ca1c4b15a02e
Refi Go <64242999+refigo@users.noreply.github.com>
2023-04-25 14:38:35 +0900

    FEAT: Set DbCommonManagerModule - #28

--- backend/src/db-manager/db-manager.module.ts
+++ backend/src/db-manager/db-manager.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { DbChatsManagerModule } from './db-chats-manager/db-chats-manager.module';
+import { DbCommonManagerModule } from './db-common-manager/db-common-manager.module';
 import { DbGamesManagerModule } from './db-games-manager/db-games-manager.module';
 import { DbUsersManagerModule } from './db-users-manager/db-users-manager.module';
 
 @Module({
   imports: [
-    DbChatsManagerModule, 
-    DbGamesManagerModule, 
-    DbUsersManagerModule
+    DbUsersManagerModule,
+    DbChatsManagerModule,
+    DbGamesManagerModule,
+    DbCommonManagerModule
   ]
 })
 export class DbManagerModule {}
