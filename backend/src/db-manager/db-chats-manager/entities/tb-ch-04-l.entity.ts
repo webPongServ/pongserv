ae684c81350e0bd7130c1b144669c78a5a5f8514
Refi Go <64242999+refigo@users.noreply.github.com>
2023-04-25 10:58:13 +0900

    FEAT: Set TB_CH04L Entity closes - #23

---
+++ backend/src/db-manager/db-chats-manager/entities/tb-ch-04-l.entity.ts
@@ -0,0 +1,47 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { TbCh01LEntity } from "./tb-ch-01-l.entity";
+
+@Entity({ name: 'TB_CH03L' })
+export class TbCh03LEntity {
+	// USER_ID
+	/* TODO - 
+		- USER_ID 생기면 FK(ManyToOne)로 넣을 예정.
+		- CHT_RM_ID와 USER_ID를 복합키로 사용해서 PK를 만들 계획.
+	*/
+	// NOTE - 우선은 PK가 필요하기 때문에 임시로 TMP_ID를 사용.
+	@PrimaryGeneratedColumn({ name: 'TMP_ID', type: 'number' })
+	tmpId: number;
+
+	// BLOCK_USER_ID
+	/* TODO - 
+		- USER_ID 생기면 FK(ManyToOne)로 넣을 예정.
+		- CHT_RM_ID와 USER_ID를 복합키로 사용해서 PK를 만들 계획.
+	*/
+	// NOTE - 우선은 PK가 필요하기 때문에 임시로 TMP_ID를 사용.
+	@PrimaryGeneratedColumn({ name: 'TMP_BLOCK_ID', type: 'number' })
+	tmpBlockId: number;
+
+	// ST_CD
+	@Column({ name: 'ST_CD', type: 'varchar', length: 2 })
+	stCd: string;
+
+	// RSST_DTTM
+	@Column({ name: 'RSST_DTTM', type: 'timestamp', precision: 0 })
+	rsstDttm: Date;
+
+	// RELE_DTTM
+	@Column({ name: 'RELE_DTTM', type: 'timestamp', precision: 0 })
+	releDttm: Date;
+
+	// DEL_TF
+	@Column({ name: 'DEL_TF', type: 'boolean' })
+	delTf: boolean;
+
+	// FRST_DTTM
+	@Column({ name: "FRST_DTTM", type: 'timestamp', precision: 6 })
+	frstDttm: Date;
+
+	// LAST_DTTM
+	@Column({ name: "LAST_DTTM", type: 'timestamp', precision: 6 })
+	lastDttm: Date;
+}
