ab6c3e577b862c4c9aa0c5224692fd3def659375
Refi Go <64242999+refigo@users.noreply.github.com>
2023-04-25 18:10:19 +0900

    FIX: Set PK in TB_GM02S

--- backend/src/db-manager/db-games-manager/entities/tb-gm-02-s.entity.ts
+++ backend/src/db-manager/db-games-manager/entities/tb-gm-02-s.entity.ts
@@ -1,28 +1,31 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
+// game statistics by user - 유저별 게임 통계
 @Entity({ name: 'TB_GM02S' })
 export class TbGm02SEntity {
 	// USER_ID
-	//TODO - add
+	//TODO - Set FK as USER_ID
+	@PrimaryColumn({ type: 'integer' })
+	tmpId: number;
 
 	// TOT_RCD
-	@Column({ name: "TOT_RCD", type: 'number' })
+	@Column({ name: "TOT_RCD", type: 'integer' })
 	totRcd: number;
 
 	// TOT_VCT
-	@Column({ name: "TOT_VCT", type: 'number' })
+	@Column({ name: "TOT_VCT", type: 'integer' })
 	totVct: number;
 
 	// TOT_DFT
-	@Column({ name: "TOT_DFT", type: 'number' })
+	@Column({ name: "TOT_DFT", type: 'integer' })
 	totDft: number;
 
 	// LLVL
-	@Column({ name: "LLVL", type: 'number' })
+	@Column({ name: "LLVL", type: 'integer' })
 	llvl: number;
 
 	// LDDR_PCENT
-	@Column({ name: "LDDR_PCENT", type: 'number' })
+	@Column({ name: "LDDR_PCENT", type: 'integer' })
 	lddrPcent: number;
 
 	// DEL_TF
