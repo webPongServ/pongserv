21480be578de6eaf32626de16506bfc50f032656
Refi Go <64242999+refigo@users.noreply.github.com>
2023-04-25 14:38:04 +0900

    FEAT: Set TB_CM01D Entity - #28

---
+++ backend/src/db-manager/db-common-manager/entities/tb-cm-01-d.entity.ts
@@ -0,0 +1,32 @@
+import { Column, Entity } from "typeorm";
+
+@Entity({ name: 'TB_CM01D' })
+export class TbCm01DEntity {
+	// CMMN_CD
+	@Column({ name: "CMMN_CD", type: 'varchar', length: 100 })
+	cmmnCd: string;
+
+	// CMMN_DTL_CD
+	@Column({ name: "CMMN_DTL_CD", type: 'varchar', length: 100 })
+	cmmnDtlCd: string;
+
+	// CMMN_CD_NM
+	@Column({ name: "CMMN_CD_NM", type: 'varchar', length: 200 })
+	cmmnCdNm: string;
+
+	// CMMN_CD_DTL_NM
+	@Column({ name: "CMMN_CD_DTL_NM", type: 'varchar', length: 200 })
+	cmmnCdDtlNm: string;
+
+	// DEL_TF
+	@Column({ name: 'DEL_TF', type: 'boolean' })
+	delTf: boolean;
+
+	// FRST_DTTM
+	@Column({ name: "FRST_DTTM", type: 'timestamp', precision: 6 })
+	frstDttm: Date;
+
+	// LAST_DTTM
+	@Column({ name: "LAST_DTTM", type: 'timestamp', precision: 6 })
+	lastDttm: Date;
+}
