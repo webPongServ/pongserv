7dba38fffe5123802374be8888b04d390af2a5b1
Refi Go <64242999+refigo@users.noreply.github.com>
2023-04-25 12:36:13 +0900

    FEAT: Set DbUsersManagerModule - #27

--- backend/src/db-manager/db-manager.module.ts
+++ backend/src/db-manager/db-manager.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DbChatsManagerModule } from './db-chats-manager/db-chats-manager.module';
 import { DbGamesManagerModule } from './db-games-manager/db-games-manager.module';
+import { DbUsersManagerModule } from './db-users-manager/db-users-manager.module';
 
 @Module({
-  imports: [DbChatsManagerModule, DbGamesManagerModule]
+  imports: [
+    DbChatsManagerModule, 
+    DbGamesManagerModule, 
+    DbUsersManagerModule
+  ]
 })
 export class DbManagerModule {}
---
+++ backend/src/db-manager/db-users-manager/db-users-manager.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+	imports: [
+	  TypeOrmModule.forFeature([
+		
+	  ]),
+	],
+	providers: []
+})
+export class DbUsersManagerModule {}
