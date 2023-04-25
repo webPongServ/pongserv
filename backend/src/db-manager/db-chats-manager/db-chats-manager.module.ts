cfe44f282361b4ee2c29f27db4331034c8bc10c3
Refi Go <64242999+refigo@users.noreply.github.com>
2023-04-25 11:13:37 +0900

    FEAT: Imports Entities in TypeORMModule.forFeature - #20

--- backend/src/db-manager/db-chats-manager/db-chats-manager.module.ts
+++ backend/src/db-manager/db-chats-manager/db-chats-manager.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TbCh01LEntity } from './entities/tb-ch-01-l.entity';
+import { TbCh02DEntity } from './entities/tb-ch-02-d.entity';
 import { TbCh02LEntity } from './entities/tb-ch-02-l.entity';
+import { TbCh03LEntity } from './entities/tb-ch-03-l.entity';
+import { TbCh04LEntity } from './entities/tb-ch-04-l.entity';
 
 @Module({
 	imports: [
 	  TypeOrmModule.forFeature([
 		TbCh01LEntity,
-		TbCh02LEntity
+		TbCh02LEntity,
+		TbCh02DEntity,
+		TbCh03LEntity,
+		TbCh04LEntity
 	  ]),
 	],
 	providers: []
