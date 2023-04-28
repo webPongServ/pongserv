0b2ddb4fccea445e573f6110ba02a95571f0f5d3
SeungWoonSong <susong@student.42seoul.kr>
2023-04-28 16:54:09 +0900

    FEAT: new package for socket.io and gateway

--- backend/src/chats/chats.module.ts
+++ backend/src/chats/chats.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { ChatsController } from './chats.controller';
+import { ChatsGateway } from './chats.gateway';
 
 @Module({
-  providers: [ChatsService],
-  controllers: [ChatsController]
+  providers: [ChatsService, ChatsGateway],
+  controllers: [ChatsController],
 })
 export class ChatsModule {}
