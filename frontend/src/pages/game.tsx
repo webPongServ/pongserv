efb8f7ca6534d6a22ba36f4bfdd720277b70ac97
luckylooky2 <luckylooky2@naver.com>
2023-05-04 00:07:10 +0900

    #30 FEAT : game waiting pagination

--- frontend/src/pages/game.tsx
+++ frontend/src/pages/game.tsx
@@ -3,39 +3,58 @@ import GameCard from "components/game/GameCard";
 import NormalGameModal from "components/game/NormalGameModal";
 import LadderGameModal from "components/game/LadderGameModal";
 import CreateGameModal from "components/game/CreateGameModal";
+import EmptyListMessage from "components/common/utils/EmptyListMessage";
 import { useSelector } from "react-redux";
 import { IRootState } from "components/common/store";
 import "styles/global.scss";
 import "styles/Game.scss";
 
-import { Box } from "@mui/system";
+import { Box, Pagination } from "@mui/material";
 import { Button } from "@mui/joy";
 
 const Game = () => {
   const [roomStatus, setRoomStatus] = useState<string>("game");
   const [selectedID, setSelectedID] = useState<string>("");
+  const [page, setPage] = useState<number>(1);
   const gameRooms = useSelector(
     (state: IRootState) => state.gameRooms.gameRooms
   );
 
   return (
     <Box id="GameWaiting" className="flex-container">
-      <Box id="game-box" className="flex-wrap-container overflow">
-        {gameRooms.map((value, index) => {
-          return (
-            <GameCard
-              key={value.id + index}
-              id={value.id}
-              title={value.title}
-              owner={value.owner}
-              maxScore={value.maxScore}
-              difficulty={value.difficulty}
-              createdAt={value.createdAt}
-              setRoomStatus={setRoomStatus}
-              setSelectedID={setSelectedID}
-            />
-          );
-        })}
+      <Box id="game-box">
+        <Box className="list flex-wrap-container">
+          {gameRooms.length === 0 ? (
+            <EmptyListMessage message="게임방이 존재하지 않습니다!" />
+          ) : (
+            gameRooms.map((value, index) =>
+              4 * (page - 1) <= index && index < 4 * page ? (
+                <GameCard
+                  key={value.id + index}
+                  id={value.id}
+                  title={value.title}
+                  owner={value.owner}
+                  maxScore={value.maxScore}
+                  difficulty={value.difficulty}
+                  createdAt={value.createdAt}
+                  setRoomStatus={setRoomStatus}
+                  setSelectedID={setSelectedID}
+                />
+              ) : null
+            )
+          )}
+        </Box>
+      </Box>
+      <Box className="pagination flex-container">
+        <Pagination
+          count={Math.floor(gameRooms.length / 4) + 1}
+          variant="outlined"
+          shape="rounded"
+          onChange={(e, number) => {
+            console.log(e);
+            setPage(number);
+          }}
+        />
       </Box>
       <Box id="button-box" className="flex-container">
         <Button onClick={() => setRoomStatus("create-game")}>
