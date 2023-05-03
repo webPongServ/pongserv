8908aa0c4a4087db15e27c22c96b474c694c782c
Refi Go <64242999+refigo@users.noreply.github.com>
2023-05-03 15:14:23 +0900

    FEAT: SEt repositories in DbUsersManagerService

--- backend/src/db-manager/db-users-manager/db-users-manager.service.ts
+++ backend/src/db-manager/db-users-manager/db-users-manager.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { TbUa01MEntity } from './entities/tb-ua-01-m.entity';
+import { TbUa01LEntity } from './entities/tb-ua-01-l.entity';
+import { TbUa02LEntity } from './entities/tb-ua-02-l.entity';
+import { TbUa03DEntity } from './entities/tb-ua-03-d.entity';
+import { TbUa03MEntity } from './entities/tb-ua-03-m.entity';
 
 @Injectable()
 export class DbUsersManagerService {
 	constructor(
 		@InjectRepository(TbUa01MEntity) private ua01mRp: Repository<TbUa01MEntity>,
-		// @InjectRepository(TbUa01LEntity) private ua01lRp: Repository<TbUa01LEntity>,
+		@InjectRepository(TbUa01LEntity) private ua01lRp: Repository<TbUa01LEntity>,
+		@InjectRepository(TbUa02LEntity) private ua02lRp: Repository<TbUa02LEntity>,
+		@InjectRepository(TbUa03MEntity) private ua03mRp: Repository<TbUa03MEntity>,
+		@InjectRepository(TbUa03DEntity) private ua03dRp: Repository<TbUa03DEntity>,
 	) { }
 
 	setUser() {
