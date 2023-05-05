f80805c010252684a4ec1ad9ddbd791f19e81056
luckylooky2 <luckylooky2@naver.com>
2023-05-05 16:06:45 +0900

    #30 FIX : re-route when waiting game

--- frontend/src/components/common/MainRoute.tsx
+++ frontend/src/components/common/MainRoute.tsx
@@ -8,25 +8,23 @@ import GameReady from "components/game/GameReady";
 
 const MainRoute = (): JSX.Element => {
   return (
-    <>
-      <Routes>
-        <Route path="/redirect" element={<Redirect />} />
-        <Route path="/profile/:nickname" element={<Profile />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/game/:id" element={<GameRoom />} />
-        <Route
-          path="/game/ladder"
-          element={
-            <GameReady
-              type="래더 게임"
-              content="적절한 상대를 찾는 중"
-              funnyImg="funny4.gif"
-            />
-          }
-        />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/redirect" element={<Redirect />} />
+      <Route path="/profile/:nickname" element={<Profile />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/game" element={<Game />} />
+      <Route path="/game/normal" element={<GameRoom />} />
+      <Route
+        path="/game/ladder"
+        element={
+          <GameReady
+            type="래더 게임"
+            content="적절한 상대를 찾는 중"
+            funnyImg="funny4.gif"
+          />
+        }
+      />
+    </Routes>
   );
 };
 
