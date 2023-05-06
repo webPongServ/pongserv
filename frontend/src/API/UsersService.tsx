e927c15a6845613268f5cf5b13f718334ae783c7
luckylooky2 <luckylooky2@naver.com>
2023-05-06 18:43:23 +0900

    #45 FEAT : getMe API

--- frontend/src/API/UsersService.tsx
+++ frontend/src/API/UsersService.tsx
@@ -5,14 +5,7 @@ export const usersURL = (path: string = ""): string => {
 };
 
 const UserService = {
-  //   login: {
-  // reissueAccessToken: async () => await refresh.patch(loginUrl(`/token`)),
-  //   },
-  //   login42: {
-  //     issueAccessToken: async (code) =>
-  //       await instance.post(loginUrl(`/token?code=${code}`)),
-  //     // reissueAccessToken: async () => await refreshft.patch(login42Url(`/token`)),
-  //   },
+  getMe: async () => await instance.get(usersURL("me")),
 };
 
 export default UserService;
