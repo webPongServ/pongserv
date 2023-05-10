5e3c22dc2a6f1abc30c6681f9f0f9b9d0251ba58
SeungwoonSong <andrew960811@naver.com>
2023-05-10 07:11:47 +0000

    FIX: use Cache layer

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
-#bin/bash
+#!/bin/bash
 
-cd /home/susong/pongserv/backend
+echo "스크립트 시작"
+cd /home/susong/pongserv
+echo "작업 디렉토리 이동 완료"
 git pull
+echo "git pull 완료"
+docker-compose down 
+echo "docker-compose down 완료"
 docker-compose build --no-cache && docker-compose up -d
+echo "docker-compose build 완료"
+
+echo "스크립트 종료"
+docker ps
