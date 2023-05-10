e88c906559dfeb5feb872e6338ec14880933181e
luckylooky2 <luckylooky2@naver.com>
2023-05-10 18:07:40 +0900

    #7 FEAT : enterance API in password form

--- frontend/src/components/common/chatting/PasswordFormCard.tsx
+++ frontend/src/components/common/chatting/PasswordFormCard.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { CurrentChattingActionTypes } from "types/redux/CurrentChatting";
 import { ChattingRoomDetail } from "types/Detail";
+import ChattingService from "API/ChattingService";
 import "styles/global.scss";
 import "styles/ChattingDrawer.scss";
 
@@ -27,6 +28,27 @@ const PasswordFormCard = (props: PasswordFormCardProps) => {
       setValue(target.value);
     }
   };
+
+  const handleSubmitPassword = async () => {
+    if (value.length === 0) return alert("비밀번호를 입력해주세요!");
+
+    const response = await ChattingService.postEntrance({
+      id: props.room.id,
+      pwd: value,
+    });
+    dispatch({
+      type: CurrentChattingActionTypes.UPDATE_STATUS_CHATTING,
+      payload: {
+        id: props.room.id,
+        chatroomName: props.room.chatroomName,
+        ownerNickname: props.room.ownerNickname,
+        type: props.room.type,
+        currentCount: props.room.currentCount,
+        maxCount: props.room.maxCount,
+      },
+    });
+  };
+
   return (
     <Card id="password-form" className="flex-container" variant="outlined">
       <Box className="content flex-container">
@@ -39,23 +61,7 @@ const PasswordFormCard = (props: PasswordFormCardProps) => {
           onChange={handlePassword}
         />
         <ButtonGroup variant="contained">
-          <Button
-            className="submit"
-            onClick={() => {
-              if (value.length === 0) return alert("비밀번호를 입력해주세요!");
-              dispatch({
-                type: CurrentChattingActionTypes.UPDATE_STATUS_CHATTING,
-                payload: {
-                  id: props.room.id,
-                  title: props.room.chatroomName,
-                  owner: props.room.ownerNickname,
-                  type: props.room.type,
-                  current: props.room.currentCount,
-                  max: props.room.maxCount,
-                },
-              });
-            }}
-          >
+          <Button className="submit" onClick={handleSubmitPassword}>
             확인
           </Button>
           <Button
