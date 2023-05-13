6e2fea44fc73776e8ae5a4daf15e0ae246a20a16
luckylooky2 <luckylooky2@naver.com>
2023-05-13 21:29:10 +0900

    #30 FEAT : game logic with relative coordination

--- frontend/src/constant.tsx
+++ frontend/src/constant.tsx
@@ -5,8 +5,10 @@ export const GameBoardConst = {
   GAMEBOARD_BORDER: 5,
   GAMEBOARD_WIDTH: 1000,
   GAMEBOARD_HEIGHT: 600,
-  PADDLE_WIDTH: 20,
-  PADDLE_HEIGHT: 150,
+  PADDLE_WIDTH: 20, // css 변경 필요
+  PADDLE_HEIGHT: 150, // css 변경 필요
+  BALL_DIAMETER: 15,
+  MOVE_PIXEL: 50,
 };
 
 export const ChattingRoomType = {
