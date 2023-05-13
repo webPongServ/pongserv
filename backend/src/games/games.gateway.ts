54fe5d7f119aace017ba3ae383cb58ba63af88f2
SeungWoonSong <susong@student.42seoul.kr>
2023-05-13 20:43:39 +0900

    FEAT: game gateway

--- backend/src/games/games.gateway.ts
+++ backend/src/games/games.gateway.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, UnauthorizedException, UseGuards } from '@nestjs/common';
 import {
   ConnectedSocket,
   OnGatewayConnection,
@@ -8,6 +8,7 @@ import {
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
+import { JwtService } from '@nestjs/jwt';
 
 @WebSocketGateway({
   cors: {
@@ -20,17 +21,48 @@ export class GamesGateway
 {
   private logger = new Logger('GameGateway');
 
-  constructor() {
+  constructor(private jwtService: JwtService) {
     this.logger.log('GameGateway constructor');
+  }
+  validateAccessToken(socket: Socket) {
+    const bearerToken = socket.handshake.headers['authorization'];
+
+    // Bearer 토큰이 아닌 경우 에러 메시지를 클라이언트에게 보냅니다.
+    if (!bearerToken || !bearerToken.startsWith('Bearer ')) {
+      socket.emit(
+        'exception',
+        'invalid Bearer',
+        'Invalid token(It is not a bearerToken)',
+      );
+      socket.disconnect();
+      return;
+    }
+
+    // Bearer 토큰에서 실제 토큰 부분만 추출합니다.
+    const token = bearerToken.split(' ')[1];
+
+    try {
+      const payload = this.jwtService.verify(token);
+      socket.data = payload.intraId;
+    } catch (e) {
+      socket.emit('exception', 'Invalid token(value in token is not valid)');
+      socket.disconnect();
+    }
   }
 
   handleConnection(@ConnectedSocket() socket: Socket) {
-    this.logger.log(`GameGateway handleConnection: ${socket.id}`);
+    this.validateAccessToken(socket);
+    this.logger.log(
+      `GameGateway handleConnection: ${socket.id} intraId : ${socket.data}`,
+    );
   }
   handleDisconnect(@ConnectedSocket() socket: Socket) {
-    this.logger.log(`GameGateway handleDisconnect: ${socket.id}`);
+    this.validateAccessToken(socket);
+    this.logger.log(
+      `GameGateway handleDisconnect: ${socket.id} intraId : ${socket.data}`,
+    );
   }
   afterInit() {
-    this.logger.log('GameGateway init');
+    this.logger.log('GameGateway init is Successful!');
   }
 }
