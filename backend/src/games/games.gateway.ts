cfc518822e155f8223f3e8162b3d00d347666c05
SeungWoonSong <susong@student.42seoul.kr>
2023-05-13 19:11:35 +0900

    FEAT: game gateway with CORS setting

--- backend/src/games/games.gateway.ts
+++ backend/src/games/games.gateway.ts
@@ -1,9 +1,38 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 
-@WebSocketGateway()
-export class GamesGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+  namespace: 'games',
+})
+export class GamesGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('GameGateway');
+
+  constructor() {
+    this.logger.log('GameGateway constructor');
+  }
+  handleDisconnect(client: any) {
+    throw new Error('Method not implemented.');
+  }
+  afterInit() {
+    this.logger.log('GameGateway init');
+  }
+
+  handleConnection() {
+    this.logger.log('GameGateway connection');
+  }
+
+  handoleDisconnect() {
+    this.logger.log('GameGateway disconnect');
   }
 }
