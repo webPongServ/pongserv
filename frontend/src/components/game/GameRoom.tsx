2257115496cd3bf86738fd240c3eb10fb418f398
luckylooky2 <luckylooky2@naver.com>
2023-05-15 17:20:35 +0900

    #30 FIX : WIP navigating

--- frontend/src/components/game/GameRoom.tsx
+++ frontend/src/components/game/GameRoom.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import GameReady from "components/game/GameReady";
 
 const GameRoom = () => {
-  // 게임 정보 받아오기 => 2명이면 setIsWaiting(false);
   const [isWaiting, setIsWaiting] = useState<boolean>(true);
 
   return isWaiting ? (
