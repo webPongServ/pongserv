303810caa490e8c868bbef7f2da226840bad8905
Refi Go <64242999+refigo@users.noreply.github.com>
2023-05-19 21:36:32 +0900

    CHORE: Modify file name for UsersChatsGateway

--- backend/src/users-chats-socket/users-chats-socket.module.ts
+++ backend/src/users-chats-socket/users-chats-socket.module.ts
@@ -1,5 +1,5 @@
 import { Module, forwardRef } from '@nestjs/common';
-import { UsersChatsGateway } from './chats.gateway';
+import { UsersChatsGateway } from './users-chats.gateway';
 import { ChatsModule } from 'src/chats/chats.module';
 import { UsersModule } from 'src/users/users.module';
 
