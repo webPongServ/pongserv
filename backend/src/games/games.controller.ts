16dbec671238d35b1ca0f470b8906bb2a777eba4
SeungWoonSong <susong@student.42seoul.kr>
2023-05-19 16:38:10 +0900

    FIX: no used removed

--- backend/src/games/games.controller.ts
+++ backend/src/games/games.controller.ts
@@ -39,66 +39,7 @@ export class GamesController {
   @Get('static/:userId')
   getGameStatic(@Param('userId') user: string) {
     console.log('userId is ', user);
+    // return this.gamesService.getGameSummary(user);
     return this.gamesService.getUserStatic(user);
-  }
-
-  @ApiResponse({
-    status: 201,
-    description: '일반 게임 생성 성공',
-    type: String, // 성공시 Gameroom_id string 반환
-  })
-  @ApiResponse({
-    status: 403,
-    description: '일반 게임 생성 실패',
-  })
-  @ApiOperation({ summary: '일반 게임 생성' })
-  @Post('/normal/create/:gameroom_name/:score/:difficulty')
-  createGame() {
-    return 'Hello World! it is createGame()';
-  }
-
-  @ApiResponse({
-    status: 201,
-    description: '일반 게임 수정 성공',
-  })
-  @ApiResponse({
-    status: 403,
-    description: '일반 게임 수정 실패',
-  })
-  @ApiOperation({ summary: '일반 게임 수정' })
-  @Patch('/normal/info/:gameroom_id/:gameroom_name/:score/:difficulty')
-  changeGameInfo() {
-    return 'Hello World! it is changeGameInfo()';
-  }
-
-  @ApiResponse({
-    status: 201,
-    description: '래더 게임 등록 성공',
-  })
-  @ApiOperation({ summary: '래더 게임 등록' })
-  @Post('/ladder/registration')
-  ladderRegistration() {
-    return 'Hello World! it is ladderRegistration()';
-  }
-
-  @ApiResponse({
-    status: 201,
-    description: '래더 게임 등록 취소 성공',
-  })
-  @ApiOperation({ summary: '래더 게임 등록 취소' })
-  @Patch('/ladder/deregistration')
-  ladderDeregistration() {
-    return 'Hello World! it is ladderDeregistration()';
-  }
-
-  @ApiResponse({
-    status: 200,
-    description: '게임 결과 화면 정보',
-    type: GameDto, // 향후 추가 많음
-  })
-  @ApiOperation({ summary: '게임 결과 화면' })
-  @Get('result/:gameroom_id')
-  getGameResult() {
-    return 'Hello World! it is getGameResult()';
   }
 }
