c6c9ac0488bf876669f0c962b13feadc84a6f0cd
SeungWoonSong <susong@student.42seoul.kr>
2023-05-20 21:53:12 +0900

    FIX: Dodge Console changed

--- backend/src/games/games.gateway.ts
+++ backend/src/games/games.gateway.ts
@@ -206,7 +206,7 @@ export class GamesGateway
 
   @SubscribeMessage('dodge')
   scoreUpdate(@ConnectedSocket() socket: Socket, @MessageBody() message: any) {
-    this.logger.log(`Dodge Game Score Update ${message.roomId}}}`);
+    this.logger.log(`Dodge Game Score Update ${message.roomId}`);
     // console.log('Dodge Game Score Update', message);
     // 필요한 데이터 roodId, Score(양쪽 다), userId
     const roomId = message.roomId;
