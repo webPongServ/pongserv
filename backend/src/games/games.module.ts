a0a3bfd135968cc1c551381eadc938a7a6456c31
SeungWoonSong <susong@student.42seoul.kr>
2023-05-20 14:04:24 +0900

    FIX : No middleware log in Gmae Moudle

--- backend/src/games/games.module.ts
+++ backend/src/games/games.module.ts
@@ -1,15 +1,13 @@
 import { DbUsersManagerModule } from 'src/db-manager/db-users-manager/db-users-manager.module';
 import { DbGamesManagerModule } from './../db-manager/db-games-manager/db-games-manager.module';
 import { HttpModule } from '@nestjs/axios';
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { GamesService } from './games.service';
 import { GamesController } from './games.controller';
-import { LoggerMiddleware } from '../common/middlewares/logger.middleware';
 import { GamesGateway } from './games.gateway';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersChatsSocketModule } from 'src/users-chats-socket/users-chats-socket.module';
-import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
@@ -31,8 +29,4 @@ import { UsersModule } from 'src/users/users.module';
   controllers: [GamesController],
   exports: [GamesService],
 })
-export class GamesModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('games');
-  }
-}
+export class GamesModule {}
