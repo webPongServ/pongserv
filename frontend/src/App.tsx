42f2208f67f50b00bf90baf3ffe96e44b473569b
luckylooky2 <luckylooky2@naver.com>
2023-05-22 19:35:19 +0900

    CHORE : remove react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,17 @@ import Home from "pages/index";
 import Redirect from "pages/Redirect";
 import AppHeader from "components/common/AppHeader";
 import "styles/global.scss";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/redirect" element={<Redirect />} />
-          <Route path="/*" element={<AppHeader />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/redirect" element={<Redirect />} />
+        <Route path="/*" element={<AppHeader />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
